4e9899b107e4e742536be9b92bc0326fbd79d400
Sophie <sophie.capricorne@gmail.com>
2021-10-04 03:21:19 +0200

    add gif

--- app/src/main/java/com/example/allmyfriends/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/allmyfriends/ui/SplashScreenActivity.kt
@@ -3,7 +3,6 @@ package com.example.allmyfriends.ui
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 @SuppressLint("CustomSplashScreen")
